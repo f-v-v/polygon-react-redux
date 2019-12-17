b58ff47cd483bb6d5cab68c29dfbd2fe49ea7bf5
Vladimir Frangoni <f_v_v@mail.ru>
2019-12-17 17:03:16 +0300

    Updates

---
+++ precache-manifest.e199b1d6d4db930d42c14c144a33df96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0443a6c3d32fc5dd5d22903bbfe57a3",
+    "url": "/polygon-react-redux/index.html"
+  },
+  {
+    "revision": "348ee52ca5af0359cfc4",
+    "url": "/polygon-react-redux/static/css/main.52077399.chunk.css"
+  },
+  {
+    "revision": "0eb30a8d4d808a97363d",
+    "url": "/polygon-react-redux/static/js/2.cd45f755.chunk.js"
+  },
+  {
+    "revision": "348ee52ca5af0359cfc4",
+    "url": "/polygon-react-redux/static/js/main.dcc3c3c5.chunk.js"
+  },
+  {
+    "revision": "937f094512489dbc63ad",
+    "url": "/polygon-react-redux/static/js/runtime-main.0d02a4ab.js"
+  }
+]);
