f845ccc2dd66882b91d2935feee60d0180d503f0
Vladimir Frangoni <f_v_v@mail.ru>
2019-12-17 19:15:47 +0300

    change for deploy on github

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './cpmponents/app';
 import {Provider} from 'react-redux';
-import {BrowserRouter as Router} from 'react-router-dom';  
+import {HashRouter as Router} from 'react-router-dom';  
+// import {BrowserRouter as Router} from 'react-router-dom';  
 import store from './store';
 // import * as serviceWorker from './serviceWorker';
 
